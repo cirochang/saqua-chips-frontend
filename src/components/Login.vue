ce94b5ea153f8853a91dc2f483be9d541c9ea26d
Ciro Chang <cirochang@live.com>
2017-11-03 00:52:04 -0200

    first commit

---
+++ src/components/Login.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="login-box">
+    <div class="login-logo">
+      <a href="../../index2.html"><b>Saqua</b>Chips</a>
+    </div>
+    <!-- /.login-logo -->
+    <div class="login-box-body">
+      <p class="login-box-msg">Logue-se para iniciar sua sessão</p>
+
+      <form action="../../index2.html" method="post">
+        <div class="form-group has-feedback">
+          <input type="email" class="form-control" placeholder="Usuário">
+          <span class="glyphicon glyphicon-user form-control-feedback"></span>
+        </div>
+        <div class="form-group has-feedback">
+          <input type="password" class="form-control" placeholder="Senha">
+          <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+        </div>
+        <div class="row">
+          <div class="col-xs-8">
+            <div class="checkbox icheck">
+              <label>
+                <input type="checkbox"> Lembrar de mim
+              </label>
+            </div>
+          </div>
+          <!-- /.col -->
+          <div class="col-xs-4">
+            <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+      <!-- /.social-auth-links -->
+    </div>
+    <!-- /.login-box-body -->
+  </div>
+</template>
+
+<script>
+
+  export default {
+
+  }
+</script>
