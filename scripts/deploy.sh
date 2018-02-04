faefc0ed0ed519dee3646ff4e0ab497dc2c3e2a6
Ciro Chang <cirochang@live.com>
2018-02-04 19:16:10 -0200

    configurable ip

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,7 @@
-scp -i ~/.ssh/id_rsa ~/Projects/saqua-chips-frontend/saqua-chips-frontend.zip pi@10.3.141.1:
-ssh -i ~/.ssh/id_rsa pi@10.3.141.1 <<'ENDSSH'
+RASP_IP="192.168.0.178" # ROUTER IP
+# RASP_IP="10.3.141.1" # RASP WIFI IP
+scp -i ~/.ssh/id_rsa ~/Projects/saqua-chips-frontend/saqua-chips-frontend.zip pi@$RASP_IP:
+ssh -i ~/.ssh/id_rsa pi@$RASP_IP <<'ENDSSH'
 rm -rf saqua-chips-frontend
 mkdir saqua-chips-frontend
 unzip -o ~/saqua-chips-frontend.zip -d ~/saqua-chips-frontend/
